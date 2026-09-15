5b0ca05acd14fd75b1f27f444f74b1ec4566d5a7
Marzuq Oyebo <timileyinishola624@gmail.com>
2026-09-15 11:10:21 +0100

    Apply database migrations on startup

--- LifeLine.WebAPI/Program.cs
+++ LifeLine.WebAPI/Program.cs
@@ -262,6 +262,10 @@ var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    await db.Database.MigrateAsync();
+
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
     var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
